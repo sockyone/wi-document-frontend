6f16dcbc07156a14c0b6e32f8d18420f17065151
unknown <sockyone16899@gmail.com>
2019-06-18 15:52:57 +0700

    fix file json sai format

--- src/js/components/PythonApi/PythonContentComponent.js
+++ src/js/components/PythonApi/PythonContentComponent.js
@@ -43,31 +43,20 @@ class PythonContent extends Component {
         }
         let lists = Object.keys(content).map(value=>{
             return (
-                <div style={{margin: "50px", borderTop: '10px', borderBottom: '10px'}}>
+                <div style={{margin: "50px", borderBottom: '3x solid'}}>
                     <h3>{value}</h3>
                     <h4>Required:</h4>
-                    
                     {Object.keys(content[value]['params']['required']).map(value2 => {
-                        if (content[value]['params']['required'])
                         return <p>{value2}: {content[value]['params']['required'][value2]}</p>
                     })}
                     <h4>Optional:</h4>
                     {Object.keys(content[value]['params']['optional']).map(value2 => {
-                        if (content[value]['params']['optional'])
                         return <p>{value2}: {content[value]['params']['optional'][value2]}</p>
                     })}
                     <h4>Returns: {content[value]['returns']}</h4>
                     <h4>Explain: {content[value]['explain']}</h4>
                     <h4>Example:</h4>
                     <p style = {{whiteSpace: 'pre'}}>{content[value]['example']}</p>
-                    {/* <h4>Explain:</h4>
-                    {Object.keys(content[value]['explain']).map(value2 => {
-                        return <p>{value2}: {content[value]['params']['required'][value2]}</p>
-                    })}
-                    <h4>Example:</h4>
-                    {Object.keys(content[value]['example']).map(value2 => {
-                        return <p>{value2}: {content[value]['params']['required'][value2]}</p>
-                    })} */}
                 </div>
             );
         })
